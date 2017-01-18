f1760ae9d8a34f61cb4a3c1a7406c862fe8d9540
ArthurOuaknine <arthur.ouaknine@gmail.com>
2017-01-18 20:07:04 +0100

    Add app local

---
+++ app_local.py
@@ -0,0 +1,51 @@
+from flask import Flask
+from flask import render_template
+from pymongo import MongoClient
+import json
+from bson import json_util
+from bson.json_util import dumps
+
+app = Flask(__name__)
+
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+DBS_NAME = 'election'
+COLLECTION_NAME = 'votes'
+#FIELDS = {'school_state': True,
+ #         'resource_type': True,
+  #        'poverty_level': True,
+   #       'date_posted': True,
+	#      'total_donations': True,
+     #     '_id': False}
+
+FIELDS = {'time': True,
+          'vote': True,
+          'nb_votes': True,
+          'state': True,
+          '_id': False}
+
+PASSWORD = open("mongopassword.txt").read()
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/election/votes")
+def donorschoose_projects():
+
+    # connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+    connection = MongoClient()
+    print()
+    collection = connection[DBS_NAME][COLLECTION_NAME]
+    votes = collection.find(projection=FIELDS, limit=100000)
+    #projects = collection.find(projection=FIELDS)
+    json_votes = []
+    for vote in votes:
+        json_votes.append(vote)
+    json_votes = json.dumps(json_votes, default=json_util.default)
+    connection.close()
+    return json_votes
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5000,debug=True)
